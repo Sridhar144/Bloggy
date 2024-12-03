007d756b10db0e46b01edbfaeedf3a2582cf8367
Sridhar <sridharsundar100@gmail.com>
2024-12-04 00:02:02 +0530

    why

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static export generation
+    distDir: 'build',
+    // output: 'export', // Enables static export generation
     images: {
       unoptimized: true, // Ensures images are compatible with static export
     },
