e8e13e4d41e756ea74209e7c31c647c27d4db196
Sridhar <sridharsundar100@gmail.com>
2024-12-03 23:12:56 +0530

    idk

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export', // Enables static export generation
+    images: {
+      unoptimized: true, // Ensures images are compatible with static export
+    },
+  };
 export default nextConfig;
